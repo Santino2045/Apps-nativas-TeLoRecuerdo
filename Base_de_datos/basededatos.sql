006f45bede5c1496a2b0a8a22ba2278104f88567
Santino <fenoysantino135@gmail.com>
2025-09-23 16:43:44 -0300

    pagina inicio

--- Base_de_datos/basededatos.sql
+++ Base_de_datos/basededatos.sql
@@ -1,53 +1,57 @@
-CREATE TABLE Paciente (id_paciente INT primary key,
-id_usuario INT,
-nombre VARCHAR(50), 
-apellido VARCHAR(50), 
-fecha_nacimiento DATE,
-contacto_Emergencia VARCHAR(20),
-FOREIGN KEY (id_usuario) REFERENCES USUARIO(id_usuario)
+CREATE TABLE Usuario ( 
+    id_usuario INT PRIMARY KEY,
+    username VARCHAR(50),
+    password VARCHAR(50),
+    fecha_creacion VARCHAR(50),
+    mail VARCHAR(50),
+    perfil VARCHAR(20) -- ('Paciente', 'Medico' o 'Familiar')
+);
+
+CREATE TABLE Paciente (
+    id_paciente INT PRIMARY KEY,
+    id_usuario INT,
+    nombre VARCHAR(50), 
+    apellido VARCHAR(50), 
+    fecha_nacimiento DATE,
+    contacto_Emergencia VARCHAR(20),
+    FOREIGN KEY (id_usuario) REFERENCES Usuario(id_usuario)
 );
 
 CREATE TABLE Familiar ( 
-id_familiar INT PRIMARY KEY,
-id_paciente INT,
-id_usuario,
-nombre VARCHAR(50), 
-Parentesco VARCHAR(50),
-contacto VARCHAR(20),
-FOREIGN KEY (id_paciente) REFERENCES PACIENTE (id_paciente)
-FOREIGN KEY (id_usuario ) REFERENCES USUARIO(id_usuario)
+    id_familiar INT PRIMARY KEY,
+    id_paciente INT,
+    id_usuario INT,
+    nombre VARCHAR(50), 
+    parentesco VARCHAR(50),
+    contacto VARCHAR(20),
+    FOREIGN KEY (id_paciente) REFERENCES Paciente(id_paciente),
+    FOREIGN KEY (id_usuario) REFERENCES Usuario(id_usuario)
 );
 
-CREATE TABLE Usuario ( 
-id_usuario INT PRIMARY KEY,
-id_paciente INT,
-nombre VARCHAR(50), 
-fecha_creacion VARCHAR(50),
-mail VARCHAR(20),
-FOREIGN KEY (id_paciente) REFERENCES PACIENTE(id_paciente)
+CREATE TABLE Medicamento ( 
+    id_medicamento INT PRIMARY KEY,
+    nombre VARCHAR(50), 
+    dosis VARCHAR(50),
+    indicaciones VARCHAR(100),
+    id_paciente INT, 
+    FOREIGN KEY (id_paciente) REFERENCES Paciente(id_paciente)
 );
 
-CREATE TABLE Medicamento ( id_medicamento INT PRIMARY KEY,
-nombre VARCHAR(50), 
-dosis VARCHAR(50),
-indicaciones VARCHAR (20),
-id_paciente INT, 
-FOREIGN KEY (id_paciente) REFERENCES PACIENTE(id_paciente)
+CREATE TABLE Medico ( 
+    id_medico INT PRIMARY KEY,
+    id_usuario INT,
+    nombre VARCHAR(50), 
+    especialidad VARCHAR(50),
+    horario_atencion VARCHAR(50),
+    FOREIGN KEY (id_usuario) REFERENCES Usuario(id_usuario)
 );
 
-CREATE TABLE Medico ( id_medico INT PRIMARY KEY,
-id_usuario INT,
-nombre VARCHAR(50), 
-especialidad VARCHAR(50),
-Horario_Atencion VARCHAR(20),
-FOREIGN KEY (id_usuario ) REFERENCES USUARIO(id_usuario)
-);
-
-CREATE TABLE Consulta (id_consulta INT PRIMARY KEY,
-id_paciente INT,
-id_medico INT,
-mensaje VARCHAR(50), 
-fecha DATE,
-FOREIGN KEY (id_paciente) REFERENCES PACIENTE(id_paciente),
-FOREIGN KEY (id_medico) REFERENCES MEDICO(id_medico)
+CREATE TABLE Consulta (
+    id_consulta INT PRIMARY KEY,
+    id_paciente INT,
+    id_medico INT,
+    mensaje VARCHAR(100), 
+    fecha DATE,
+    FOREIGN KEY (id_paciente) REFERENCES Paciente(id_paciente),
+    FOREIGN KEY (id_medico) REFERENCES Medico(id_medico)
 );
--- Codigofuente/Frontend/TeLoRecuerdo/src/app/Componentes/pagina-inicio/pagina-inicio.component.ts
+++ Codigofuente/Frontend/TeLoRecuerdo/src/app/Componentes/pagina-inicio/pagina-inicio.component.ts
@@ -1,14 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { IonicModule, NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-pagina-inicio',
   templateUrl: './pagina-inicio.component.html',
   styleUrls: ['./pagina-inicio.component.scss'],
+  imports:[IonicModule]
 })
-export class PaginaInicioComponent  implements OnInit {
+export class PaginaInicioComponent {
+  fechaHoy: string;
 
-  constructor() { }
+  constructor(private navCtrl: NavController) {
+    const hoy = new Date();
+    this.fechaHoy = hoy.toLocaleDateString();
+  }
 
-  ngOnInit() {}
+  irAMedicamentos() {
+    this.navCtrl.navigateForward('/tab1');
+  }
 
+  contactarMedico() {
+    this.navCtrl.navigateForward('/tab2');
+  }
+
+  comprarMedicamentos() {
+    this.navCtrl.navigateForward('/tab3');
+  }
+
+  contactarFamiliar() {
+    this.navCtrl.navigateForward('/tab4');
+  }
+
+  enviarSOS() {
+    alert('SOS enviado');
+  }
 }
